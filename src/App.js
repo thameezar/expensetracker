03ea1ef16799269a72773531cafa5c9bdec6b1f6
Thameeza <thameezanaaz@gmail.com>
2025-03-17 23:02:36 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Home from './pages/Home/Home';
 
-function App() {
+export const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Home />
     </div>
   );
 }
 
 export default App;
+
+/*
+import Home from './pages/Home/Home'
+import { SnackbarProvider } from 'notistack';
+
+function App() {
+  return (
+    <SnackbarProvider >
+      <div>
+        <Home />
+      </div>
+    </SnackbarProvider>
+  );
+}
+
+export default App;
+*/
